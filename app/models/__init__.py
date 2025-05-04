236e55eabbeb68f1c5d7cdfb90b0a2efaddfb293
callmeJerish <AkoBanul20@gmail.com>
2025-05-04 20:48:49 +0800

    remove adoption table

--- app/models/__init__.py
+++ app/models/__init__.py
@@ -1,7 +1,7 @@
 from .user import User
-from .pet import Pet, LostPet
+from .pet import Pet, LostPet, AdoptionPet, AdoptionPetViews
 from .lost_pet_report import LostPetReport
 from .notification import Notification
 
 
-__all__ = ["User", "Pet", "LostPet", "LostPetReport", "Notification"]
+__all__ = ["User", "Pet", "LostPet", "LostPetReport", "Notification", "AdoptionPet", "AdoptionPetViews",]
