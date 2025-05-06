5400112e7e1c7225f67312ced3f672d123d11251
callmeJerish <AkoBanul20@gmail.com>
2025-05-06 09:07:07 +0800

    add adoption in init

--- app/models/__init__.py
+++ app/models/__init__.py
@@ -2,6 +2,16 @@ from .user import User
 from .pet import Pet, LostPet, AdoptionPet, AdoptionPetViews
 from .lost_pet_report import LostPetReport
 from .notification import Notification
+from .adoption import Adoption
 
 
-__all__ = ["User", "Pet", "LostPet", "LostPetReport", "Notification", "AdoptionPet", "AdoptionPetViews",]
+__all__ = [
+    "User",
+    "Pet",
+    "LostPet",
+    "LostPetReport",
+    "Notification",
+    "AdoptionPet",
+    "AdoptionPetViews",
+    "Adoption",
+]
